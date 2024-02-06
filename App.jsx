c1fd71ab13a52ec7e1dcb3dac4b584b48934ca97
ingrid <kamilaingridc@gmail.com>
2024-02-06 08:40:51 -0300

    class

---
+++ App.jsx
@@ -0,0 +1,15 @@
+import { View, // importa componentes do módulo react
+    Text, 
+    StyleSheet, 
+    TextInput  // caixa de entrada
+ } from "react-native";
+
+export default function App(){  // define função
+    return(  // inicia o retorno da função // view parecido com div
+        <View>  
+            <View></View>
+            <View></View>
+            <View></View>
+        </View>
+    )
+}
