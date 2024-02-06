706935163859cad0f3dab612b811dfc94eb0ed7d
ingrid <kamilaingridc@gmail.com>
2024-02-06 10:44:34 -0300

    class

--- componentes/login/index.jsx
+++ componentes/login/index.jsx
@@ -1,14 +1,46 @@
-import { View, Text, TextInput } from "react-native";
+import { View, Text, TextInput, Button, TouchableOpacity } from "react-native";
 import styles from "./style";
+import React, {useState} from "react";
 
 export default function Login(){  // define função
+
+    const [user, setUser] = useState('')  // setar usuário
+    const [password, setPassword] = useState('')  // setar senha
+
+
     return(  // inicia o retorno da função // view parecido com div
         <View style={styles.container}>  
             <View style={styles.box}>
                 <Text style={styles.title}>Login</Text>
             </View>
-            <View></View>
-            <View></View>
+            <View style={styles.box}>
+                <TextInput 
+                    style={styles.InputBox}
+                    placeholder="User"
+                    onChangeText={setUser}
+                    value={user}
+                >
+
+                </TextInput>
+            </View>
+            <View style={styles.box}>
+                <TextInput 
+                    style={styles.InputBox}
+                    placeholder="Password"
+                    onChangeText={setPassword}
+                    value={password}
+                >
+
+                </TextInput>
+            </View>
+
+
+            <View style={styles.box}>
+                <TouchableOpacity style={styles.Button}>
+                    <Text style={{color:'#fff', fontSize:20}}> Enter</Text>
+                </TouchableOpacity>
+            </View>
+
         </View>
     )
 }
--- componentes/login/style.jsx
+++ componentes/login/style.jsx
@@ -5,19 +5,42 @@ const styles = StyleSheet.create({
         flex: 1,
         alignItems: 'center',
         justifyContent: 'center',
-        backgroundColor:'#66d985'
+        backgroundColor:'#000'
     },
     title:{
         fontSize: 30,
-        fontWeight: 'bold'
+        fontWeight: 'bold',
+        color:'#DF1C46'
     },
     box:{
         alignItems:'center',
-        justifyContent:'center'
-    },
+        justifyContent:'center',
+        width:'90%'
+        },
     InputBox:{
-        borderColor:'#000'
+        backgroundColor: '#2c2c2c',
+        color:'#fff',
+        borderColor:'#DF1C46',
+        borderWidth: 1,
+        marginTop: 20,
+        width:'80%',
+        height:30,
+        borderRadius:10,
+        padding: 10,
     },
-}) 
+
+    Button:{
+        alignItems:'center',
+        justifyContent:'center',
+        width:'50%',
+        height: 40,
+        backgroundColor: '#DF1C46',
+        marginTop: 20,
+        borderRadius:10,
+        padding: 10,
+        borderColor:'#fff',
+        borderWidth: 1
+    }
+})
 
 export default styles
